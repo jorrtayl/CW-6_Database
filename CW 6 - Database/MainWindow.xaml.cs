0f114c84714ac663d420eb0fda0ae1f4ab334a2b
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-02-22 20:00:41 -0600

    Finished Assignment

--- CW 6 - Database/MainWindow.xaml.cs
+++ CW 6 - Database/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,119 @@ namespace CW_6___Database
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
+        string EmployeesDBConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\EmployeesDB.accdb";
+
         public MainWindow()
         {
             InitializeComponent();
+            cn = new OleDbConnection(EmployeesDBConnectionString);
+        }
+
+        private void btnAssets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select* from Assets";
+            string data = "";
+
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            while (read.Read())
+            {
+                data += read[0].ToString() + "\n";
+            }
+        }
+
+        private void ShowAssets(object sender, RoutedEventArgs e)
+        {
+            // Needed for the connection
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+            // Open the connection
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+
+            // Hold the data from the database
+            string data = "";
+
+            // Show all fields for Assets Table
+            if (A_EmployeeID.Text == "EmployeeID")
+            {
+                while (read.Read())
+                {
+                    data += read[0].ToString() + "\n";
+                }
+
+                A_EmployeeID.Text = data;
+            }
+            else if (A_AssetID.Text == "AssetID")
+            {
+                while (read.Read())
+                {
+                    data += read[1].ToString() + "\n";
+                }
+
+                A_AssetID.Text = data;
+            }
+            else if (A_Description.Text == "Description")
+            {
+                while (read.Read())
+                {
+                    data += read[2].ToString() + "\n";
+                }
+
+                A_Description.Text = data;
+            }
+
+            // Close the connection
+            cn.Close();
+        }
+
+        private void ShowEmployees(object sender, RoutedEventArgs e)
+        {
+            // Needed for the connection
+            string query = "select * from Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+            // Open the connection
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+
+            // Hold the data from the database
+            string data = "";
+
+            // Show all fields for Employees Table
+            if (E_EmployeeID.Text == "EmployeeID")
+            {
+                while (read.Read())
+                {
+                    data += read[0].ToString() + "\n";
+                }
+
+                E_EmployeeID.Text = data;
+            }
+            else if (E_LastName.Text == "LastName")
+            {
+                while (read.Read())
+                {
+                    data += read[1].ToString() + "\n";
+                }
+
+                E_LastName.Text = data;
+            }
+            else if (E_FirstName.Text == "FirstName")
+            {
+                while (read.Read())
+                {
+                    data += read[2].ToString() + "\n";
+                }
+
+                E_FirstName.Text = data;
+            }
+
+            // Close the connection
+            cn.Close();
         }
     }
 }
